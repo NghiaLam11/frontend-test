0213699bdf88b91232a7fed04b7e80749e6d0cc1
hiennghia11 <lamhiennghia1110@gmail.com>
2023-03-02 22:09:26 +0700

    Responsive

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const feature = document.querySelector(".feature");
 const featureImg = document.querySelector(".feature__img");
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > feature.offsetHeight && window.scrollY < 3000) {
     console.log("OKKKK");
-    featureImg.style.right = 8 + "rem";
+    if (window.innerWidth < 1024) {
+      featureImg.style.right = 4 + "rem";
+    } else {
+      featureImg.style.right = 8 + "rem";
+    }
   } else {
     featureImg.style.right = -100 + "rem";
   }
@@ -11,14 +16,17 @@ window.addEventListener("scroll", () => {
 
 const end = document.querySelector(".end");
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(end.scrollWidth);
-  if (window.scrollY > 2600 && window.scrollY < 4200) {
+  if (window.scrollY > end.offsetHeight && window.scrollY < 4200) {
+    console.log("OKKKK");
+    end.style.left = 0 + "rem";
+    end.style.opacity = 1;
+  } else  if (window.scrollY > 2600  && window.scrollY < 4200) {
     console.log("OKKKK");
     end.style.left = 0 + "rem";
     end.style.opacity = 1;
   } else {
     end.style.opacity = 0.5;
     end.style.left = 180 + "rem";
-  }
+  } 
 });
+console.log(end.offsetHeight)
